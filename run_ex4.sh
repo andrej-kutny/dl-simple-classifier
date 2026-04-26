d06b1df017f60d15e1145b92c4dd9b9d55bcfb2b
kuan25gq <kuan25gq@student.ju.se>
2026-04-26 20:02:38 +0000

    separate run 3 and 4

---
+++ run_ex4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=ex4
+#SBATCH --output=logs/%j_ex4.out
+#SBATCH --error=logs/%j_ex4.err
+#SBATCH --time=0-03:59:59
+#SBATCH --gres=gpu:1
+#SBATCH --partition=priority
+
+mkdir -p logs
+
+export TF_GPU_ALLOCATOR=cuda_malloc_async
+
+# usage: sbatch run_ex3_ex4.sh path/to/model_final.keras -e 25
+# example of how to follow the logs
+# tail logs/687_ex3_ex4.out --follow
+
+python python_files/ex4.py "$@"
